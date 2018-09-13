302daf0484c981693dbeab25a3fe48faefe90373
TheodoreLv <1145281480@qq.com>
2018-09-13 19:08:31 +0800

    优化pc页面排版，新增写日志气泡

--- js/slide.js
+++ js/slide.js
@@ -1,59 +1,53 @@
-$().ready(function () {
-    var $slides = $(".slide,.slide_hidden");
-    var curpos = $slides.length / 2 - 1;
-    $(".slide_prev").bind("click",function (){
-        if ((curpos - 1) >= 0){
-            curpos--;
-        }else{
-        	curpos = $slides.length - 1;
-        }
-        switch_slide(curpos);
+$(function () {
+    const $slides = $(".slide,.slide_hidden");
+    let curpos = $slides.length / 2 - 1;
+    $(".slide_prev").on("click",function (){
+        curpos = prev_slide(1);
+        switch_slide();
     });
-    $(".slide_next").bind("click",function (){
-        if ((curpos + 1) < $slides.length){
-            curpos++;
-        }else{
-        	curpos = 0;
-        }
-        switch_slide(curpos);
+    $(".slide_next").on("click",function (){
+        curpos = next_slide(1);
+        switch_slide();
     });
-    function switch_slide(index) {
-        if (index === 0){
-            rm_class();
-            $($slides.get(index)).toggleClass("slide_m");
-            $($slides.get(index + 1)).toggleClass("slide_r1");
-            $($slides.get(index + 2)).toggleClass("slide_r2");
-            $($slides.get($slides.length - 1)).toggleClass("slide_l1");
-            $($slides.get($slides.length - 2)).toggleClass("slide_l2");
-        }else if(index === 1){
-            rm_class();
-            $($slides.get(index)).toggleClass("slide_m");
-            $($slides.get(index + 1)).toggleClass("slide_r1");
-            $($slides.get(index + 2)).toggleClass("slide_r2");
-            $($slides.get(index - 1)).toggleClass("slide_l1");
-            $($slides.get($slides.length - 1)).toggleClass("slide_l2");
-        }else if(index === ($slides.length - 2)){
-            rm_class();
-            $($slides.get(index)).toggleClass("slide_m");
-            $($slides.get(index + 1)).toggleClass("slide_r1");
-            $($slides.get(0)).toggleClass("slide_r2");
-            $($slides.get(index - 1)).toggleClass("slide_l1");
-            $($slides.get(index - 2)).toggleClass("slide_l2");
-        }else if(index === ($slides.length - 1)){
-            rm_class();
-            $($slides.get(index)).toggleClass("slide_m");
-            $($slides.get(0)).toggleClass("slide_r1");
-            $($slides.get(1)).toggleClass("slide_r2");
-            $($slides.get(index - 1)).toggleClass("slide_l1");
-            $($slides.get(index - 2)).toggleClass("slide_l2");
+    function prev_slide(n){
+        if ((curpos - n) >= 0){
+            return curpos - n;
         }else{
-            rm_class();
-            $($slides.get(index)).addClass("slide_m");
-            $($slides.get(index + 1)).addClass("slide_r1");
-            $($slides.get(index + 2)).addClass("slide_r2");
-            $($slides.get(index - 1)).addClass("slide_l1");
-            $($slides.get(index - 2)).addClass("slide_l2");
+            return ($slides.length + curpos - n);
         }
+    }
+    function next_slide(n){
+        if ((curpos + n) < $slides.length){
+            return curpos + n;
+        }else{
+            return (curpos + n - $slides.length);
+        }
+    }
+    function switch_slide() {
+        rm_class();
+        let l2 = prev_slide(2);
+        let l1 = prev_slide(1);
+        let r1 = next_slide(1);
+        let r2 = next_slide(2);
+        $slides.each(function (i,$thisDiv){
+            switch (i) {
+                case l2:
+                    $($thisDiv).toggleClass("slide_l2");
+                    break;
+                case l1:
+                    $($thisDiv).toggleClass("slide_l1");
+                    break;
+                case curpos:
+                    $($thisDiv).toggleClass("slide_m");
+                    break;
+                case r1:
+                    $($thisDiv).toggleClass("slide_r1");
+                    break;
+                case r2:
+                    $($thisDiv).toggleClass("slide_r2");
+                    break;
+            }
+        })
     }
     function rm_class(){
         $(".slide_l2").toggleClass("slide_l2");
